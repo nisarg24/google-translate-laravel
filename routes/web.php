15519a06e8b713312b8352e1509bf666676c0913
Nisarg Bhavsar <nisargbhavsar24@gmail.com>
2022-04-28 13:24:32 +0530

    #2:Rapid api key updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TranslatorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TranslatorController::class, 'index']);
+Route::post('/translate', [TranslatorController::class, 'translate']);
